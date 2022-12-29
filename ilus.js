5678f4708ca91008dce8122e4ce4106a18f0ef48
dontbanimtoocool <99411897+dontbanimtoocool@users.noreply.github.com>
2022-12-28 20:40:54 -0500

    Create ilus.js

---
+++ ilus.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('body')[0].remove();
+document.write('<video loop width="540" height="960" autoplay><source src="https://cdn.discordapp.com/attachments/881555868058005514/1057834318170431488/video0.mp4" type="video/mp4"></video>');
